bd325bb2f9fb24682fd32389646a834fead69066
Rod Vagg <rod@vagg.org>
2021-03-11 15:33:41 +1100

    fix: added more type data to Token

--- cborg.d.ts
+++ cborg.d.ts
@@ -21,6 +21,7 @@ export function decode (data: Uint8Array, options?: DecodeOptions): any
 export function encode (data: any, options?: EncodeOptions): Uint8Array
 
 export class Token {
+    constructor (type: Type, value: any, encodedLength?: number)
     type: Type
     value: any
     encodedLength: number
@@ -31,4 +32,17 @@ export class Type {
     majorEncoded: number
     name: string
     terminal: boolean
+    static uint: Type
+    static negint: Type
+    static bytes: Type
+    static string: Type
+    static array: Type
+    static map: Type
+    static tag: Type
+    static float: Type
+    static false: Type
+    static true: Type
+    static null: Type
+    static undefined: Type
+    static break: Type
 }
