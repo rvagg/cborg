cb9a747857b177237180a6f80fcdcf275a9f6708
Rod Vagg <rod@vagg.org>
2023-09-11 13:43:05 +1000

    chore: recompile types

--- types/lib/json/json.d.ts
+++ types/lib/json/json.d.ts
@@ -1,5 +1,6 @@
 import { encode } from './encode.js';
 import { decode } from './decode.js';
+import { decodeFirst } from './decode.js';
 import { Tokenizer } from './decode.js';
-export { encode, decode, Tokenizer };
+export { encode, decode, decodeFirst, Tokenizer };
 //# sourceMappingURL=json.d.ts.map
