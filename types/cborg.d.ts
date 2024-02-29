986035dbffbc682b06d7bfecb8d2c3cc02048429
Rod Vagg <rod@vagg.org>
2024-03-01 10:46:54 +1100

    fix: update tyupes for new exports

--- types/cborg.d.ts
+++ types/cborg.d.ts
@@ -16,8 +16,10 @@ export type DecodeOptions = import('./interface').DecodeOptions;
 export type EncodeOptions = import('./interface').EncodeOptions;
 import { decode } from './lib/decode.js';
 import { decodeFirst } from './lib/decode.js';
+import { Tokeniser } from './lib/decode.js';
+import { tokensToObject } from './lib/decode.js';
 import { encode } from './lib/encode.js';
 import { Token } from './lib/token.js';
 import { Type } from './lib/token.js';
-export { decode, decodeFirst, encode, Token, Type };
+export { decode, decodeFirst, Tokeniser as Tokenizer, tokensToObject, encode, Token, Type };
 //# sourceMappingURL=cborg.d.ts.map
