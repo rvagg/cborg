5b8de32567063eb92f87b12036f75dac0fc50e81
Yarchik <spoko.dev@gmail.com>
2026-06-24 01:51:52 +0100

    fix: emit shortest-form floats in rfc8949EncodeOptions (RFC 8949 4.2.1)

--- test/test-encode-rfc8949.js
+++ test/test-encode-rfc8949.js
@@ -27,6 +27,17 @@ describe('RFC8949 deterministic encoding', () => {
     // {10: 10, 100: 100, -1: -1, "z": "z", "aa": "aa", false: false}
   })
 
+  it('should encode floats in shortest form', () => {
+    // https://datatracker.ietf.org/doc/html/rfc8949#section-4.2.1
+    // "Floating-point values also MUST use the shortest form ... 1.5 is
+    //  encoded as 0xf93e00"
+    assert.deepEqual(toHex(encode(1.5, rfc8949EncodeOptions)), 'f93e00')
+    assert.deepEqual(toHex(encode(Infinity, rfc8949EncodeOptions)), 'f97c00')
+    assert.deepEqual(toHex(encode(NaN, rfc8949EncodeOptions)), 'f97e00')
+    // values that genuinely need 64 bits stay 64 bits
+    assert.deepEqual(toHex(encode(1.1, rfc8949EncodeOptions)), 'fb3ff199999999999a')
+  })
+
   it('will throw on complex key types', () => {
     const value = new Map()
     // https://datatracker.ietf.org/doc/html/rfc8949#section-4.2.1
